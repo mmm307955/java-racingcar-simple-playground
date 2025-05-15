e9c340560e87e50a6630b27fc2d45256c9cc2caf
minji <mmm307955@gmail.com>
2025-05-15 17:21:33 +0900

    2단계 자동차경주-우승자 로직 수정

--- src/main/java/Winners.java
+++ src/main/java/Winners.java
@@ -3,10 +3,16 @@ import java.util.stream.Collectors;
 
 public class Winners {
 
-    public static List<String> findWinnersNames(List<Car> cars) {
-        return cars.stream()
-                .filter(car -> car.getPosition() >= 5)
-                .map(Car::getCarName)
-                .collect(Collectors.toList());
-    }
+  public static List<String> findWinnersNames(List<Car> cars) {
+
+    int maxPosition = cars.stream()
+        .mapToInt(Car::getPosition)
+        .max()
+        .orElse(0);//값이 비어있을 때는 우승 위치로 0을 사용
+
+    return cars.stream()
+        .filter(car -> car.getPosition() == maxPosition)
+        .map(Car::getCarName)
+        .collect(Collectors.toList());
+  }
 }
